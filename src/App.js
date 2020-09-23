34e8cd84fa62e7d46709cee96f2070f791175de8
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-09-23 09:35:51 -0700

    blank canvas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>After Workz</h1>
-      </header>
+        <h1>Fresh Start</h1>
     </div>
   );
 }
