04c104ba19589e22c5c59b8fb94b8712ca13ef6b
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-10-02 15:35:00 -0700

    adding components and styles

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if(country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
   try {
-      
-    const response = await axios.get(url);
-    console.log(response)
     const {
-      data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+      data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
     const modifiedData = { confirmed, recovered, deaths, lastUpdate };
 
